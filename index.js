d31bcefb6822bcad729730d41fcc6a66bb979687
Michael Kim <m.kim4247@gmail.com>
2020-05-16 20:40:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
 // Code your solutions in this file
 // Code your solutions in this file
-function writeCards(names, event) {
-  for (let i = 0; i < names.length; i++);
-  console.log(`Thanks you, ${names[i]}, for the wonderful gift!);
-}
-
