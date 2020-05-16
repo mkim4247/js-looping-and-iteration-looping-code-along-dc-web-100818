cef1cab7c40c1491fba573a863d7a88305752420
Michael Kim <m.kim4247@gmail.com>
2020-05-16 20:45:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 function writeCards(names, event) {
   for (let i = 0; i < names.length; i++);
-  console.log(`Thank you, ${names[i]}, for the wonderful gift!`);
+  console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
 }
 writeCards(["Ada", "Brendan", "Ali"], "birthday");
 
